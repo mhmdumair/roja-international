758cf452f6064676377f08e5867055f9732193df
mhmdumair <umairzahiraj16@gmail.com>
2026-03-16 22:52:47 +0530

    fix: routing issue

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { NextRequest, NextResponse } from "next/server";
 import { requireAdmin } from "@/lib/adminAuth";
 import { getUploadParams, deleteImg } from "@/lib/cloudinary";
